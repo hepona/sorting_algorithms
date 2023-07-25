9465af3146ee6631f55c44efd3ff9f48bbdc3ae5
Rahif hiba <57334931+hepona@users.noreply.github.com>
2023-07-25 19:28:44 +0100

    DONE

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,17 +6,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j;
-int tmp = 0;
-for (i = 0; i < size - 1; i++)
-{
-for (j = 0; j < size - i - 1; j++)
-if (array[j] > array[j + 1])
-{
-tmp = array[j];
-array[j] = array[j + 1];
-array[j + 1] = tmp;
-print_array(array, size);
-}
-}
+    size_t i, j;
+    int tmp = 0;
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+                print_array(array, size);
+            }
+        }
+    }
 }
