25869443fcf9c7524ecc18227199d1579be0fc41
Rahif hiba <57334931+hepona@users.noreply.github.com>
2023-07-26 02:12:32 +0100

    insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,12 +9,14 @@ void insertion_sort_list(listint_t **list)
     listint_t *tmp;
     int i;
 
-    for (i = 0; *list != NULL; i++)
+    for (i = 0; (*list)->next != NULL; i++)
     {
         tmp = (*list);
         if (tmp->n > tmp->next->n && tmp->next->n > tmp->prev->n) /*so (tmp->n) > (tmp->next)->n > (tmp->prev->n)*/
         {
-            tmp->prev = 
+            tmp->prev = tmp;
+            tmp->next = tmp->prev;
+            print_list(tmp);
         }
     }
     
